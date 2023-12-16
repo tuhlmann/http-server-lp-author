a7003a538f5ba8e6f1a2f5a234a0f20b7702008f
Torsten Uhlmann <tuhlmann@agynamix.de>
2023-12-16 13:50:41 +0100

    Change move/src/main.rs to return the modified String from borrow() fn.

--- move/src/main.rs
+++ move/src/main.rs
@@ -1,25 +1,26 @@
 fn main() {
-  let hello : String = String::from("Hello");
-  take(hello); // From take(): Hello WasmEdge!
-  // The following will fail since hello is already taken by take() and no longer available here
-  // println!("From main(): {}", hello);
+    let hello: String = String::from("Hello");
+    take(hello); // From take(): Hello WasmEdge!
+                 // The following will fail since hello is already taken by take() and no longer available here
+                 // println!("From main(): {}", hello);
 
-  let hello : String = String::from("Hello");
-  take(hello.clone()); // From take(): Hello WasmEdge!
-  println!("From main(): {}", hello); // From main(): Hello
+    let hello: String = String::from("Hello");
+    take(hello.clone()); // From take(): Hello WasmEdge!
+    println!("From main(): {}", hello); // From main(): Hello
 
-  let hello : String = String::from("Hello");
-  borrow(&hello); // From borrow(): Hello WasmEdge!
-  println!("From main(): {}", hello); // From main(): Hello
+    let mut hello: String = String::from("Hello");
+    hello = borrow(&hello); // From borrow(): Hello WasmEdge!
+    println!("From main(): {}", hello); // From main(): Hello
 }
 
-fn take (mut s: String) {
-  s.push_str(" WasmEdge!");
-  println!("From take(): {}", s);
+fn take(mut s: String) {
+    s.push_str(" WasmEdge!");
+    println!("From take(): {}", s);
 }
 
-fn borrow (s: &String) {
-  let mut buf = String::from(s);
-  buf.push_str(" WasmEdge!");
-  println!("From borrow(): {}", buf);
+fn borrow(s: &String) -> String {
+    let mut buf = String::from(s);
+    buf.push_str(" WasmEdge!");
+    println!("From borrow(): {}", buf);
+    buf
 }
